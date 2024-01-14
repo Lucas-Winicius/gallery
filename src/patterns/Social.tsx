7d0a86010b5104b698911df343f7082899274c56
Lucas-Winicius <lucasclucas7@gmail.com>
2024-01-14 11:53:06 -0300

    flickr added

--- src/patterns/Social.tsx
+++ src/patterns/Social.tsx
@@ -1,10 +1,15 @@
 "use client";
 import { GithubLogo, InstagramLogo, LinkedinLogo } from "@phosphor-icons/react";
+import Image from "next/image";
 import Link from "next/link";
+import flickrIcon from "@/images/flickrIcon.svg"
 
 export default function Social() {
   return (
     <div className="flex gap-3">
+      <Link target="_blank" href="https://www.flickr.com/photos/199594124@N07/">
+        <Image src={flickrIcon} alt="Flickr Icon" width={23} height={23} />
+      </Link>
       <Link target="_blank" href="https://www.linkedin.com/in/lucas-winicius-souza/">
         <LinkedinLogo size={23} weight="bold" color="white" />
       </Link>
