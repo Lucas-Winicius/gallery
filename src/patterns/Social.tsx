e71eb4151f9b21eac8b7fd1bb689fca21922a327
Lucas-Winicius <lucasclucas7@gmail.com>
2024-01-14 16:49:01 -0300

    sociel titles

--- src/patterns/Social.tsx
+++ src/patterns/Social.tsx
@@ -6,19 +6,32 @@ import { GitHubIcon, InstagramIcon, LinkedinIcon } from "./phosphoricons";
 export default function Social() {
   return (
     <div className="hidden gap-3 sm:flex">
-      <Link target="_blank" href="https://www.flickr.com/photos/199594124@N07/">
+      <Link
+        title="Flickr - Lucas Winicius"
+        target="_blank"
+        href="https://www.flickr.com/photos/199594124@N07/"
+      >
         <Image src={flickrIcon} alt="Flickr Icon" width={23} height={23} />
       </Link>
       <Link
+        title="Linkedin - Lucas Winicius"
         target="_blank"
         href="https://www.linkedin.com/in/lucas-winicius-souza/"
       >
         <LinkedinIcon />
       </Link>
-      <Link target="_blank" href="https://github.com/Lucas-Winicius">
+      <Link
+        title="GitHub - Lucas Winicius"
+        target="_blank"
+        href="https://github.com/Lucas-Winicius"
+      >
         <GitHubIcon />
       </Link>
-      <Link target="_blank" href="https://www.instagram.com/sr_pumpkin_/">
+      <Link
+        title="Instagram - Sr. Pumpkin ( Lucas Winicius )"
+        target="_blank"
+        href="https://www.instagram.com/sr_pumpkin_/"
+      >
         <InstagramIcon />
       </Link>
     </div>
