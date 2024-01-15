1b84591fcbe8c563ece5019ff588be7ab338fd50
Lucas-Winicius <lucasclucas7@gmail.com>
2024-01-15 15:53:08 -0300

    api request

---
+++ src/@types/Picture.d.ts
@@ -0,0 +1,9 @@
+type Picture = {
+  id: String;
+  name: String;
+  description: String;
+  url: String;
+  tags: String[];
+  createdAt: Date;
+  updatedAt: Date;
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,27 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+"use client";
+import { useEffect, useState } from "react";
+
 export default function Home() {
-  return (
-    <main className="flex-grow">
-      CONTEUDO
-    </main>
-  );
+  const [pictures, setPictures] = useState<Picture[] | number>(0);
+
+  useEffect(() => {
+    const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+
+    fetch(`${apiUrl}/gallery`)
+      .then((res) => res.json())
+      .then((data) => setPictures(data))
+      .catch(() => setPictures(1))
+      .finally(() => console.log(pictures));
+  }, []);
+
+  if (pictures == 0) {
+    return <h1>VAZIO</h1>;
+  }
+
+  if (pictures == 1) {
+    return <h1>ERRO</h1>;
+  }
+
+  return <main className="flex-grow">{JSON.stringify(pictures)}</main>;
 }
