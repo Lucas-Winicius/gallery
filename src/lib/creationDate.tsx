37e18a0a3f1779e9cee66fb92f1afdcd1c4e5661
Lucas-Winicius <lucasclucas7@gmail.com>
2024-04-06 19:16:02 -0300

    date on portuguese

--- src/lib/creationDate.tsx
+++ src/lib/creationDate.tsx
@@ -1,6 +1,8 @@
 import moment from "moment";
+import "moment/locale/pt-br";
 
 export default function creationDate(date: string | number | Date) {
+  moment.locale("pt-BR");
   const message = moment(date).fromNow();
   return message;
 }
