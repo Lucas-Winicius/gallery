88316b7f83386b6b94929387ea15bc2d866905c5
Lucas-Winicius <lucasclucas7@gmail.com>
2024-04-07 16:42:36 -0300

    end of maintenance

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,8 +2,8 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  if (!request.nextUrl.pathname.startsWith("/maintenance"))
-    return NextResponse.redirect(new URL("/maintenance", request.url));
+  //if (!request.nextUrl.pathname.startsWith("/maintenance"))
+  //  return NextResponse.redirect(new URL("/maintenance", request.url));
 
   NextResponse.next();
 }
