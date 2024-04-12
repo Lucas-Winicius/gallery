89c8b0a171a663e1572b7bcdd83bce1dba5c7013
Lucas-Winicius <lucasclucas7@gmail.com>
2024-04-12 09:45:46 -0300

    applying the individual image page

--- src/app/image/[id]/page.tsx
+++ src/app/image/[id]/page.tsx
@@ -1,4 +1,7 @@
+/* eslint-disable @next/next/no-img-element */
 import creationDate from "@/lib/creationDate";
+import { ArrowLeftIcon } from "@/patterns/phosphoricons";
+import Link from "next/link";
 
 export default async function Image({ params }: { params: { id: string } }) {
   const apiUrl = process.env.NEXT_PUBLIC_API_URL;
@@ -12,15 +15,25 @@ export default async function Image({ params }: { params: { id: string } }) {
   if (picture.statusCode) return <h1>Foto não encontrada.</h1>;
 
   return (
-    <div className="flex md:flex-row flex-col items-center justify-around w-full mx-3 space-x-7 space-y-7 my-5">
-      <div className="min-w-fit">
-        <img src={picture.url} alt={picture.title} className="h-[120vw] md:h-[80vh]" />
+    <div className="flex md:flex-row flex-col items-center justify-around mx-3 space-x-7 space-y-7 my-5">
+      <div className="flex items-center justify-center w-fit h-full">
+        <img
+          src={picture.url}
+          alt={picture.title}
+          className="w-auto lg:w-4/5"
+        />
       </div>
       <div className="flex flex-col justify-between space-y-5">
         <h1 className="font-extrabold text-3xl">{picture.name}</h1>
         <p className="font-medium text-lg">{picture.description}</p>
         <p>{picture.tags.join(", ")}</p>
-        <p className="text-sm text-zinc-700">{creationDate(picture.createdAt)}</p>
+        <p className="text-sm text-zinc-700">
+          {creationDate(picture.createdAt)}
+        </p>
+        <Link href="/" className="flex justify-center items-center space-x-2 bg-zinc-950 w-max text-white px-3 py-1 rounded font-bold">
+          <ArrowLeftIcon size={16} />
+          <p>Voltar</p>
+        </Link>
       </div>
     </div>
   );
--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,52 +1,17 @@
-import {
-  Dialog,
-  DialogClose,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
-import creationDate from "@/lib/creationDate";
+/* eslint-disable @next/next/no-img-element */
+import Link from "next/link";
 
 export default function Gallery({ pictures }: { pictures?: Picture[] }) {
   return (
     <main className="grid grid-cols-2 md:grid-cols-3 gap-2 m-2 flex-grow">
       {pictures?.map((picture) => (
-        <Dialog key={picture.id}>
-          <DialogTrigger asChild>
-            <button key={picture.id} className="aspect-ratio-1x1">
-              <img
-                src={picture.url}
-                alt={picture.name}
-                className="w-full h-full object-cover rounded-lg transition-transform flex justify-center items-center bg-slate-300 hover:scale-95"
-              />
-            </button>
-          </DialogTrigger>
-          <DialogContent className="sm:max-w-md">
-            <DialogHeader>
-              <DialogTitle>{picture.name}</DialogTitle>
-              <DialogDescription>{picture.description}</DialogDescription>
-            </DialogHeader>
-            <div className="flex items-center justify-center">
-              <img
-                src={picture.url}
-                alt={picture.name}
-                className="rounded max-h-64"
-              />
-            </div>
-            <DialogFooter className="sm:justify-start">
-              <DialogClose asChild>
-                <div className="flex gap-2 text-sm font-light text-zinc-600">
-                  <p>{creationDate(picture.createdAt)}</p>
-                  <span> - </span>
-                  <p>{picture.tags.join(", ")}</p>
-                </div>
-              </DialogClose>
-            </DialogFooter>
-          </DialogContent>
-        </Dialog>
+        <Link href={`/image/${picture.id}`} key={picture.id} className="aspect-ratio-1x1">
+          <img
+            src={picture.url}
+            alt={picture.name}
+            className="w-full h-full object-cover rounded-lg transition-transform flex justify-center items-center bg-slate-300 hover:scale-95"
+          />
+        </Link>
       ))}
     </main>
   );
