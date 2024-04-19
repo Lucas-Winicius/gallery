c1f6cc7324fc2b6c3af87a7bf9f524648d159a25
Lucas-Winicius <lucasclucas7@gmail.com>
2024-04-19 14:41:30 -0300

    back button

--- src/app/image/[id]/components/BackButton.tsx
+++ src/app/image/[id]/components/BackButton.tsx
@@ -1,14 +1,22 @@
+"use client";
 import { ArrowLeftIcon } from "@/patterns/phosphoricons";
+import { useRouter } from "next/navigation";
 import Link from "next/link";
 
 export default function BackButton() {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.back();
+  };
+
   return (
-    <Link
-      href="/"
+    <button
       className="flex justify-center items-center space-x-2 bg-zinc-950 w-max text-white px-3 py-1 rounded font-bold"
+      onClick={handleClick}
     >
       <ArrowLeftIcon size={16} />
       <p>Voltar</p>
-    </Link>
+    </button>
   );
 }
